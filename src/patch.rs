83db73907deced773c20efdb8817493d41698a80
unscore <130622945+justunscored@users.noreply.github.com>
2025-10-08 21:59:02 -0500

    change pid path functionality
    Use data path instead for the pid file.
    This allows gmodpatchtool to work on distributions that the binary is in an immutable location, ex: nixos and nix stores.

--- src/patch.rs
+++ src/patch.rs
@@ -687,7 +687,9 @@ where
 	let sys = System::new_all();
 
 	// Abort if another instance is already running
-	let pid_path = extend_pathbuf_and_return(std::env::current_exe().unwrap().parent().unwrap().to_path_buf(), &["gmodpatchtool.pid"]);
+	let gmptdata_path = dirs::data_dir().map(|path| path.join("GModPatchTool")).unwrap_or_else(|| PathBuf::from("."));
+	tokio::fs::create_dir_all(&gmptdata_path).await;
+    let pid_path = gmptdata_path.join("gmodpatchtool.pid");
 	let running_instance_pid = tokio::fs::read_to_string(&pid_path).await;
 	if let Ok(pid) = running_instance_pid {
 		if let Ok(pid) = pid.parse::<usize>() {
