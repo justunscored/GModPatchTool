0f114becf5a668357b7953e2e91dde02c0d32d94
unscore <130622945+justunscored@users.noreply.github.com>
2025-11-02 01:34:29 -0500

    oops again

--- src/patch.rs
+++ src/patch.rs
@@ -685,12 +685,11 @@ where
 {
 	let now = Instant::now();
 	let sys = System::new_all();
-};
 	
 	// Abort if another instance is already running
 	let pid_path = extend_pathbuf_and_return(std::env::current_exe().unwrap().parent().unwrap().to_path_buf(), &["gmodpatchtool.pid"]);
-    let pid_write_result = tokio::fs::write(&pid_path, std::process::id().to_string());
-    if pid_write_result.is_err() {
+	let pid_write_result = tokio::fs::write(&pid_path, std::process::id().to_string());
+	if pid_write_result.is_err() {
 		let pid_dir = dirs::data_dir().map(|path| path.join("GModPatchTool")).unwrap();
         tokio::fs::create_dir_all(&pid_dir);
         let pid_path = pid_dir.join("gmodpatchtool.pid");
