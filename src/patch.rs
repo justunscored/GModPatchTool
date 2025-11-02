e5b2cb50a41a7c365d8b99e85857058280256bc6
unscore <130622945+justunscored@users.noreply.github.com>
2025-11-02 01:12:49 -0500

    Update patch.rs

--- src/patch.rs
+++ src/patch.rs
@@ -689,14 +689,12 @@ where
 	
 	// Abort if another instance is already running
 	let pid_path = extend_pathbuf_and_return(std::env::current_exe().unwrap().parent().unwrap().to_path_buf(), &["gmodpatchtool.pid"]);
-    let pid_write_result = match std::fs::write(&pid_path, std::process::id().to_string()).or_else(|e| {
-				Ok(pid_write_result) => pid_write_result,
-				Err(error) => {
-	    			let pid_dir = dirs::data_dir().map(|path| path.join("GModPatchTool")).unwrap();
-        			tokio::fs::create_dir_all(&pid_dir);
-        			let pid_path = pid_dir.join("gmodpatchtool.pid")
-	 			};
-			});
+    let pid_write_result = std::fs::write(&pid_path, std::process::id().to_string());
+    if pid_write_result.is_err() {
+	    let pid_dir = dirs::data_dir().map(|path| path.join("GModPatchTool")).unwrap();
+        tokio::fs::create_dir_all(&pid_dir);
+        let pid_path = pid_dir.join("gmodpatchtool.pid")
+	 };
     //if let Err(error) = pid_write_result {
     //  return Err(AlmightyError::Generic(format!("Failed to create gmodpatchtool.pid in binary directory: {error}")))
     //}
